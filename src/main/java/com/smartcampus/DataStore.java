eff766745766ec9500461c02ca259acd63820996
Prashanthy <amalanprashanthy@gmail.com>
2026-04-15 20:21:49 +0530

    Part 1: Project setup

---
+++ src/main/java/com/smartcampus/DataStore.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus;
+
+/**
+ *
+ * @author DELL
+ */
+import com.smartcampus.model.Room;
+import com.smartcampus.model.Sensor;
+import com.smartcampus.model.SensorReading;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataStore {
+
+    public static final Map<String, Room> rooms = new HashMap<>();
+    public static final Map<String, Sensor> sensors = new HashMap<>();
+    public static final Map<String, List<SensorReading>> readings = new HashMap<>();
+}
